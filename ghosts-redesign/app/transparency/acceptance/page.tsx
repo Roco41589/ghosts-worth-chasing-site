2bfa9fafe452f1295674ba988a954713e2310c1a
Roco41589 <irishchaos415@yahoo.com>
2026-02-23 22:10:03 -0500

    Update page.tsx

--- ghosts-redesign/app/transparency/acceptance/page.tsx
+++ ghosts-redesign/app/transparency/acceptance/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useEffect, useRef } from "react";
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid, LineChart, Line, Legend } from "recharts";
 
